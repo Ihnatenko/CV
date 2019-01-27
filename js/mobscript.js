c648b9e790613a5a52796bdbf02bf0e0f84a1ad9
Ihnatenko Vitalii <ignatenko.9999@gmail.com>
2019-01-27 19:59:55 +0200

    New year

--- js/mobscript.js
+++ js/mobscript.js
@@ -1,82 +1,84 @@
-var timerGoTop;
-$(".goTop").click(function(){
+function mobile() {
 
-	var step = window.pageYOffset/100;
+	var timerGoTop;
+	$(".goTop").click(function(){
 
-	clearInterval(timerGoTop);
-	timerGoTop = setInterval(function(){
-		window.scrollTo(0, window.pageYOffset - step);
-		console.log(step);
-		console.log(window.pageYOffset);
-		if(Math.abs(window.pageYOffset) <= Math.abs(step))
+		var step = window.pageYOffset/100;
+
+		clearInterval(timerGoTop);
+		timerGoTop = setInterval(function(){
+			window.scrollTo(0, window.pageYOffset - step);
+			console.log(step);
+			console.log(window.pageYOffset);
+			if(Math.abs(window.pageYOffset) <= Math.abs(step))
+			{
+				clearInterval(timerGoTop);
+				window.scrollTo(0, 0);
+			}
+		}, 10);
+	});
+
+	//Переключення кнопки включення меню
+	$(".bt_menu").toggle(
+		function()
 		{
-			clearInterval(timerGoTop);
+			$("nav").css({"visibility": "visible"});
+			setTimeout(function(){
+				$("nav").css({"opacity": "1"});
+			}, 0);
 
-			window.scrollTo(0, 0);
+			$(".bt_menu img").css({"height": "0%"});
+			$(".bt_menu").css({"background-color": "#aaaaaa"});
+			setTimeout(function(){
+				$(".bt_menu img").attr("src","img/arrows_down.png");
+				$(".bt_menu img").css({"height": "19px"});
+			}, 100);
+
+			setTimeout(function(){
+				$("section").css({"display": "none"})
+			}, 200);
+		},
+
+		function ()
+		{
+			$("section").css({"display": ""})
+			setTimeout(function(){
+				$("nav").css({"visibility": "hidden"});
+			}, 200);
+			setTimeout(function(){
+				$("nav").css({"opacity": "0"});
+			}, 0);
+
+			$(".bt_menu img").css({"height": "0%"});
+			$(".bt_menu").css({"background-color": "#182153"});
+			setTimeout(function(){
+				$(".bt_menu img").css({"height": "19"});
+				$(".bt_menu img").attr("src","img/arrows_top.png");
+			}, 100);
 		}
-	}, 10);
+	);
 
-});
+	$("nav li").click(function(event){
+		var click_event = new Event("click");
+		document.querySelector(".bt_menu").dispatchEvent(click_event);
 
-//Переключення кнопки включення меню
-$(".bt_menu").toggle(
-	function()
-	{
-		$("nav").css({"visibility": "visible"});
-		setTimeout(function(){
-			$("nav").css({"opacity": "1"});
-		}, 0);
-
-		$(".bt_menu img").css({"height": "0%"});
-		$(".bt_menu").css({"background-color": "#aaaaaa"});
-		setTimeout(function(){
-			$(".bt_menu img").attr("src","img/arrows_down.png");
-			$(".bt_menu img").css({"height": "19px"});
-		}, 100);
-
-		setTimeout(function(){
-			$("section").css({"display": "none"})
-		}, 200);
-	},
-
-	function ()
-	{
-		$("section").css({"display": ""})
-		setTimeout(function(){
-			$("nav").css({"visibility": "hidden"});
-		}, 200);
-		setTimeout(function(){
-			$("nav").css({"opacity": "0"});
-		}, 0);
-
-		$(".bt_menu img").css({"height": "0%"});
-		$(".bt_menu").css({"background-color": "#182153"});
-		setTimeout(function(){
-			$(".bt_menu img").css({"height": "19"});
-			$(".bt_menu img").attr("src","img/arrows_top.png");
-		}, 100);
-	}
-);
-
-$("nav li").click(function(event){
-	var click_event = new Event("click");
-	document.querySelector(".bt_menu").dispatchEvent(click_event);
-
-	var elems = [experience, education, works, skills, contacts];
-	var elem = elems[event.target.getAttribute("data-num")];
-	window.scrollTo(0, elem.offsetTop);
-});
+		var elems = [experience, education, works, skills, contacts];
+		var elem = elems[event.target.getAttribute("data-num")];
+		window.scrollTo(0, elem.offsetTop);
+	});
 
 
-var timerScroll;
+	var timerScroll;
 
-window.onscroll = function() {
+	window.addEventListener('scroll', function() {
 
-	$(".goTop").css({"opacity": "1", "visibility": "visible"});
+		$(".goTop").css({"opacity": "1", "visibility": "visible"});
 
-	clearTimeout(timerScroll);
-	timerScroll = setTimeout(function(){
-		$(".goTop").css({"opacity": "0", "visibility": "hidden"});
-	}, 4000);
+		clearTimeout(timerScroll);
+		timerScroll = setTimeout(function(){
+			$(".goTop").css({"opacity": "0", "visibility": "hidden"});
+		}, 4000);
 
-};
+	});
+
+}
--- js/script.js
+++ js/script.js
@@ -1,143 +1,143 @@
 
-setTimeout(function(){
-	$("#main img").css({"opacity": "1", "margin": "0px"});
-	$("#main_info").css({"opacity": "1", "margin": "0px"});
-}, 500);
+function desktop() {
 
-var timerId;
+	setTimeout(function(){
+		$("#main img").css({"opacity": "1", "margin": "0px"});
+		$("#main_info").css({"opacity": "1", "margin": "0px"});
+	}, 500);
 
-// обработчик для меню: плавне переміщення по пунктам сайта
-$("header li").click(function(event){
+	var timerId;
 
-	var elems = [experience, education, works, skills, contacts];
-	var elem = elems[event.target.getAttribute("data-num")];
-	menu_scroll(elem);
+	// обработчик для меню: плавне переміщення по пунктам сайта
+	$("header li").click(function(event){
+
+		var elems = [experience, education, works, skills, contacts];
+		var elem = elems[event.target.getAttribute("data-num")];
+		menu_scroll(elem);
 
 
-	function menu_scroll(elem) {
-		var elemTop = elem.offsetTop - 100;
-		if((elemTop + document.documentElement.clientHeight) > document.body.clientHeight)
-		{
-			elemTop = document.body.clientHeight - document.documentElement.clientHeight;
-		}
-		//var winScroll = document.body.scrollTop;
-		var step = (elemTop - window.pageYOffset)/100;
+		function menu_scroll(elem) {
+			var elemTop = elem.offsetTop - 100;
+			if((elemTop + document.documentElement.clientHeight) > document.body.clientHeight)
+			{
+				elemTop = document.body.clientHeight - document.documentElement.clientHeight;
+			}
+			
+			var step = (elemTop - window.pageYOffset)/100;
 
-		clearInterval(timerId);
+			clearInterval(timerId);
+			timerId = setInterval(function(){
+				window.scrollTo(0, window.pageYOffset + step);
+				console.log(timerId);
+
+				if(Math.abs(window.pageYOffset - elemTop) <= Math.abs(step))
+				{
+					clearInterval(timerId);
+
+					//document.body.scrollTop = elemTop;
+					window.scrollTo(0, elemTop);
+				}
+			}, 10);
+
+			return(true);
+		};
+
+	});
+
+	// обработчик для назви сайту: плавне переміщення на початок сайту
+	$("header p").click(function(event){
+
+		var step = window.pageYOffset/100;
+
 		timerId = setInterval(function(){
-			window.scrollTo(0, window.pageYOffset + step);
-			console.log(timerId);
-			//document.body.scrollTop += step;
-			if(Math.abs(window.pageYOffset - elemTop) <= Math.abs(step))
+			window.scrollTo(0, window.pageYOffset - step);
+			if(Math.abs(window.pageYOffset) < Math.abs(step))
 			{
 				clearInterval(timerId);
-
-				//document.body.scrollTop = elemTop;
-				window.scrollTo(0, elemTop);
+				window.scrollTo(0, 0);
 			}
 		}, 10);
 
-		return(true);
-	};
 
-});
+	});
 
-// обработчик для назви сайту: плавне переміщення на початок сайту
-$("header p").click(function(event){
 
-	//var winScroll = document.body.scrollTop;
-	var step = window.pageYOffset/100;
+	window.addEventListener('scroll', function() {
 
-	timerId = setInterval(function(){
-		//document.body.scrollTop -= step;
-		window.scrollTo(0, window.pageYOffset - step);
-		if(Math.abs(window.pageYOffset) < Math.abs(step))
+		// обработчик для меню: зміна кольору пунктів при переміщенні по сайту
+		var elems = [experience, education, works, skills, contacts];
+		var elemsTop = [experience.offsetTop - 110,
+						education.offsetTop - 110,
+						works.offsetTop - 110,
+						skills.offsetTop - 110,
+						document.body.clientHeight - document.documentElement.clientHeight];
+		var winScroll = window.pageYOffset;
+
+		switch(true)
 		{
-			clearInterval(timerId);
-			window.scrollTo(0, 0);
+			case (winScroll < elemsTop[0]):
+				backColor();
+				break;
+			case ((winScroll > elemsTop[0]) && (winScroll < elemsTop[1])):
+				backColor();
+				$($("header li")[0]).css({color: "#DBDDEC"});
+				break;
+			case ((winScroll > elemsTop[1]) && (winScroll < elemsTop[2])):
+				backColor();
+				$($("header li")[1]).css({color: "#DBDDEC"});
+				break;
+			case ((winScroll > elemsTop[2]) && (winScroll < elemsTop[3])):
+				backColor();
+				$($("header li")[2]).css({color: "#DBDDEC"});
+				break;
+			case ((winScroll > elemsTop[3]) && (winScroll < elemsTop[4])):
+				backColor();
+				$($("header li")[3]).css({color: "#DBDDEC"});
+				break;
+			case (winScroll === (document.body.clientHeight - document.documentElement.clientHeight)):
+				backColor();
+				$($("header li")[4]).css({color: "#DBDDEC"});
+				break;
 		}
-	}, 10);
 
 
-});
-
-
-window.onscroll = function() {
-
-	// обработчик для меню: зміна кольору пунктів при переміщенні по сайту
-	var elems = [experience, education, works, skills, contacts];
-	var elemsTop = [experience.offsetTop - 110,
-					education.offsetTop - 110,
-					works.offsetTop - 110,
-					skills.offsetTop - 110,
-					document.body.clientHeight - document.documentElement.clientHeight];
-	var winScroll = window.pageYOffset;
-
-	switch(true)
-	{
-		case (winScroll < elemsTop[0]):
-			backColor();
-			break;
-		case ((winScroll > elemsTop[0]) && (winScroll < elemsTop[1])):
-			backColor();
-			$($("header li")[0]).css({color: "#DBDDEC"});
-			break;
-		case ((winScroll > elemsTop[1]) && (winScroll < elemsTop[2])):
-			backColor();
-			$($("header li")[1]).css({color: "#DBDDEC"});
-			break;
-		case ((winScroll > elemsTop[2]) && (winScroll < elemsTop[3])):
-			backColor();
-			$($("header li")[2]).css({color: "#DBDDEC"});
-			break;
-		case ((winScroll > elemsTop[3]) && (winScroll < elemsTop[4])):
-			backColor();
-			$($("header li")[3]).css({color: "#DBDDEC"});
-			break;
-		case (winScroll === (document.body.clientHeight - document.documentElement.clientHeight)):
-			backColor();
-			$($("header li")[4]).css({color: "#DBDDEC"});
-			break;
-	}
-
-
-	function backColor()
-	{
-		for(var i = 0;  i < 5; i++)
+		function backColor()
 		{
-			$($("header li")[i]).css({color: ""});
+			for(var i = 0;  i < 5; i++)
+			{
+				$($("header li")[i]).css({color: ""});
+			}
+			return(true);
 		}
-		return(true);
-	}
 
-	//Виклик аніміцій
+		//Виклик аніміцій
 
-	startAnimateElem(experience);
-	startAnimateElem(education);
-	startAnimateElem(works);
+		startAnimateElem(experience);
+		startAnimateElem(education);
+		startAnimateElem(works);
 
-	function startAnimateElem(elem)
-	{
-		if((winScroll + document.documentElement.clientHeight/2) > elem.offsetTop)
+		function startAnimateElem(elem)
 		{
-			$(elem).find(".info_tab_deckription").css({"left": "0px", "opacity": "1"});
-			$(elem).find(".info_tab_year").css({"right": "0px", "opacity": "1"});
-			$(elem).find(".line").css({"opacity": "1"});
-			$(elem).find(".point").css({"top": "0", "opacity": "1"});
+			if((winScroll + document.documentElement.clientHeight/2) > elem.offsetTop)
+			{
+				$(elem).find(".info_tab_deckription").css({"left": "0px", "opacity": "1"});
+				$(elem).find(".info_tab_year").css({"right": "0px", "opacity": "1"});
+				$(elem).find(".line").css({"opacity": "1"});
+				$(elem).find(".point").css({"top": "0", "opacity": "1"});
+			}
 		}
-	}
 
+		if((winScroll + document.documentElement.clientHeight/2) > skills.offsetTop)
+		{
+			setTimeout(function(){$($(".skills_tab > div")[0]).animate({width: "410px"}, 1000);}, 100);
+			setTimeout(function(){$($(".skills_tab > div")[1]).animate({width: "410px"}, 1000);}, 400);
+			setTimeout(function(){$($(".skills_tab > div")[2]).animate({width: "410px"}, 1000);}, 300);
+			setTimeout(function(){$($(".skills_tab > div")[3]).animate({width: "410px"}, 1000);}, 600);
+			setTimeout(function(){$($(".skills_tab > div")[4]).animate({width: "410px"}, 1000);}, 500);
+			setTimeout(function(){$($(".skills_tab > div")[5]).animate({width: "410px"}, 1000);}, 800);
+			setTimeout(function(){$($(".skills_tab > div")[6]).animate({width: "410px"}, 1000);}, 900);
+			setTimeout(function(){$($(".skills_tab > div")[7]).animate({width: "410px"}, 1000);}, 700);
+		}
+	});
 
-
-	if((winScroll + document.documentElement.clientHeight/2) > skills.offsetTop)
-	{
-		setTimeout(function(){$($(".skills_tab > div")[0]).animate({width: "410px"}, 1000);}, 100);
-		setTimeout(function(){$($(".skills_tab > div")[1]).animate({width: "410px"}, 1000);}, 400);
-		setTimeout(function(){$($(".skills_tab > div")[2]).animate({width: "410px"}, 1000);}, 300);
-		setTimeout(function(){$($(".skills_tab > div")[3]).animate({width: "410px"}, 1000);}, 600);
-		setTimeout(function(){$($(".skills_tab > div")[4]).animate({width: "410px"}, 1000);}, 500);
-		setTimeout(function(){$($(".skills_tab > div")[5]).animate({width: "410px"}, 1000);}, 800);
-		setTimeout(function(){$($(".skills_tab > div")[6]).animate({width: "410px"}, 1000);}, 900);
-		setTimeout(function(){$($(".skills_tab > div")[7]).animate({width: "410px"}, 1000);}, 700);
-	}
-};
+}
